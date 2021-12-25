dd9ea96f4af942f637a79129aee97fd9b2f5fd9b
NohaNxD <96643842+NohaNxD@users.noreply.github.com>
2021-12-25 11:30:13 +0530

    Add files via upload

---
+++ 2ndtest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a,b;
+    printf("enter the number a\n");
+    scanf("%d", &a);
+
+    printf("enter the number b\n");
+    scanf("%d",&b);
+    
+    printf("%d \n",a);
+    printf("%d \n",b);
+
+    printf("the program is completely executed\n");
+    return 0;
+
+}
---
+++ challenge.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+
+// starting the challenge give by codeWithHarry
+
+
+/*
+Program created by :- NohaNx
+Purpose :- Do the Multiplication of the Number given by the user
+Date :- 25th december 2021
+
+
+*/
+
+int main()
+{
+    int a;
+
+
+    // taking the no. from the user which have to be multiplied 
+
+    printf("Enter the Number you want the Multiplication of:-\n");
+    scanf("%d",&a);
+    printf("The Multiplication of %d is:-\n",a);
+
+
+
+    // multiplying the number given by the user up to 10 
+
+    printf("%d x 1 = %d\n",a,a*1);
+
+    printf("%d x 2 = %d\n",a,a*2);
+
+    printf("%d x 3 = %d\n",a,a*3);
+
+    printf("%d x 4 = %d\n",a,a*4);
+
+    printf("%d x 5 = %d\n",a,a*5);
+
+    printf("%d x 6 = %d\n",a,a*6);
+
+    printf("%d x 7 = %d\n",a,a*7);
+
+    printf("%d x 8 = %d\n",a,a*8);
+
+    printf("%d x 9 = %d\n",a,a*9);
+
+    printf("%d x 10 = %d\n",a,a*10);
+
+
+    // just for fun this line was added 
+
+    printf("Your Multiplication was Done\nIt wasn't That Hard LOL..!!\n");
+
+
+}
